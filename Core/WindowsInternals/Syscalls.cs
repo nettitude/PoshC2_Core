0b30b83de1b87f3479c3075f8844d40e8ce69f7e
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2022-03-09 14:42:04 +0000

    Be explicit about each version

--- Core/WindowsInternals/Syscalls.cs
+++ Core/WindowsInternals/Syscalls.cs
@@ -7,18 +7,10 @@ namespace Core.WindowsInternals
     {
         private enum WindowsVersions
         {
-            Server2016 = 14393,
-            Server2012_R2 = 9600,
-            Windows7_SP1 = 7601,
-            Windows10_1809 = 17763,
-            Windows10_1803 = 17134,
-            Windows10_1903 = 18362,
-            Windows10_1909 = 18363,
-            Windows10_2004 = 19041,
-            Windows10_20H2 = 19042,
-            Windows10_21H1 = 19043,
-            Windows10_21H2 = 19044,
-            Windows10_21H22 = 22000
+            _7601,
+            _9600,
+            _14393,
+            _22000,
         }
 
         internal enum SysCalls
@@ -47,21 +39,22 @@ namespace Core.WindowsInternals
             if (osVersionInfo.dwPlatformId != Internals.VER_PLATFORM_WIN32_NT)
             {
                 throw new NotSupportedException(
-                    $"Unsupported Platform ID: {osVersionInfo.dwPlatformId}, get your friendly neighbourhood developer to update the syscalls in core.");
+                    $"Unsupported Platform ID: {osVersionInfo.dwPlatformId}, run syscallsextractor and get your friendly neighbourhood developer to update the syscalls in core.");
             }
 
+            // These syscall numbers are the number "+1" to help avoid static detections, so when adding/updating numbers you need to add one to the real number
             return (byte)(osVersionInfo.dwBuildNumber switch
             {
-                (int)WindowsVersions.Windows10_21H22 => syscallType switch
+                (int)WindowsVersions._22000 => syscallType switch
                 {
-                    SysCalls.NtOpenProcess => 039,
-                    SysCalls.NtCreateThreadEx => 198,
-                    SysCalls.NtWriteVirtualMemory => 059,
-                    SysCalls.ZwAllocateVirtualMemory => 025,
-                    SysCalls.NtCreateSection => 075,
-                    SysCalls.ZwMapViewOfSection => 041,
-                    SysCalls.NtCreateProcess => 189,
-                    SysCalls.ZwProtectVirtualMemory => 081,
+                    SysCalls.NtOpenProcess => 0x27,
+                    SysCalls.NtCreateThreadEx => 0xc6,
+                    SysCalls.NtWriteVirtualMemory => 0x3b,
+                    SysCalls.ZwAllocateVirtualMemory => 0x19,
+                    SysCalls.NtCreateSection => 0x4b,
+                    SysCalls.ZwMapViewOfSection => 0x29,
+                    SysCalls.NtCreateProcess => 0xbd,
+                    SysCalls.ZwProtectVirtualMemory => 0x51,
                     SysCalls.ZwReadVirtualMemory => 0x40,
                     SysCalls.NtCreateThread => 0x4f,
                     SysCalls.NtUnmapViewOfSection => 0x2b,
@@ -70,56 +63,16 @@ namespace Core.WindowsInternals
                     SysCalls.NtQueueApcThread => 0x46,
                     _ => throw new ArgumentException($"Unknown Syscall type: {syscallType}")
                 },
-                (int)WindowsVersions.Windows10_20H2 or
-                    (int)WindowsVersions.Windows10_2004 or
-                    (int)WindowsVersions.Windows10_21H1 or
-                    (int)WindowsVersions.Windows10_21H2 => syscallType switch
-                    {
-                        SysCalls.NtOpenProcess => 039,
-                        SysCalls.NtCreateThreadEx => 194,
-                        SysCalls.NtWriteVirtualMemory => 059,
-                        SysCalls.ZwAllocateVirtualMemory => 025,
-                        SysCalls.NtCreateSection => 075,
-                        SysCalls.ZwMapViewOfSection => 041,
-                        SysCalls.NtCreateProcess => 186,
-                        SysCalls.ZwProtectVirtualMemory => 081,
-                        SysCalls.ZwReadVirtualMemory => 0x40,
-                        SysCalls.NtCreateThread => 0x4f,
-                        SysCalls.NtUnmapViewOfSection => 0x2b,
-                        SysCalls.NtCreateUserProcess => 0xc9,
-                        SysCalls.ZwFreeVirtualMemory => 0x1f,
-                        SysCalls.NtQueueApcThread => 0x46,
-                        _ => throw new ArgumentException($"Unknown Syscall type: {syscallType}")
-                    },
-                (int)WindowsVersions.Windows10_1903 or
-                    (int)WindowsVersions.Windows10_1909 => syscallType switch
-                    {
-                        SysCalls.NtOpenProcess => 039,
-                        SysCalls.NtCreateThreadEx => 190,
-                        SysCalls.NtWriteVirtualMemory => 059,
-                        SysCalls.ZwAllocateVirtualMemory => 025,
-                        SysCalls.NtCreateSection => 075,
-                        SysCalls.ZwMapViewOfSection => 041,
-                        SysCalls.NtCreateProcess => 182,
-                        SysCalls.ZwProtectVirtualMemory => 081,
-                        SysCalls.ZwReadVirtualMemory => 0x40,
-                        SysCalls.NtCreateThread => 0x4f,
-                        SysCalls.NtUnmapViewOfSection => 0x2b,
-                        SysCalls.NtCreateUserProcess => 0xc5,
-                        SysCalls.ZwFreeVirtualMemory => 0x1f,
-                        SysCalls.NtQueueApcThread => 0x46,
-                        _ => throw new ArgumentException($"Unknown Syscall type: {syscallType}")
-                    },
-                (int)WindowsVersions.Windows10_1809 => syscallType switch
+                (int)WindowsVersions._14393 => syscallType switch
                 {
-                    SysCalls.NtOpenProcess => 039,
-                    SysCalls.NtCreateThreadEx => 189,
-                    SysCalls.NtWriteVirtualMemory => 059,
-                    SysCalls.ZwAllocateVirtualMemory => 025,
-                    SysCalls.NtCreateSection => 075,
-                    SysCalls.ZwMapViewOfSection => 041,
-                    SysCalls.NtCreateProcess => 181,
-                    SysCalls.ZwProtectVirtualMemory => 081,
+                    SysCalls.NtOpenProcess => 0x27,
+                    SysCalls.NtCreateThreadEx => 0xb7,
+                    SysCalls.NtWriteVirtualMemory => 0x3b,
+                    SysCalls.ZwAllocateVirtualMemory => 0x19,
+                    SysCalls.NtCreateSection => 0x4b,
+                    SysCalls.ZwMapViewOfSection => 0x29,
+                    SysCalls.NtCreateProcess => 0xb5,
+                    SysCalls.ZwProtectVirtualMemory => 0x51,
                     SysCalls.ZwReadVirtualMemory => 0x40,
                     SysCalls.NtCreateThread => 0x4f,
                     SysCalls.NtUnmapViewOfSection => 0x2b,
@@ -128,52 +81,16 @@ namespace Core.WindowsInternals
                     SysCalls.NtQueueApcThread => 0x46,
                     _ => throw new ArgumentException($"Unknown Syscall type: {syscallType}")
                 },
-                (int)WindowsVersions.Windows10_1803 => syscallType switch
+                (int)WindowsVersions._9600 => syscallType switch
                 {
-                    SysCalls.NtOpenProcess => 039,
-                    SysCalls.NtCreateThreadEx => 188,
-                    SysCalls.NtWriteVirtualMemory => 059,
-                    SysCalls.ZwAllocateVirtualMemory => 025,
-                    SysCalls.NtCreateSection => 075,
-                    SysCalls.ZwMapViewOfSection => 041,
-                    SysCalls.NtCreateProcess => 181,
-                    SysCalls.ZwProtectVirtualMemory => 081,
-                    SysCalls.ZwReadVirtualMemory => 0x40,
-                    SysCalls.NtCreateThread => 0x4f,
-                    SysCalls.NtUnmapViewOfSection => 0x2b,
-                    SysCalls.NtCreateUserProcess => 0xc3,
-                    SysCalls.ZwFreeVirtualMemory => 0x1f,
-                    SysCalls.NtQueueApcThread => 0x46,
-                    _ => throw new ArgumentException($"Unknown Syscall type: {syscallType}")
-                },
-                (int)WindowsVersions.Server2016 => syscallType switch
-                {
-                    SysCalls.NtOpenProcess => 039,
-                    SysCalls.NtCreateThreadEx => 183,
-                    SysCalls.NtWriteVirtualMemory => 059,
-                    SysCalls.ZwAllocateVirtualMemory => 025,
-                    SysCalls.NtCreateSection => 075,
-                    SysCalls.ZwMapViewOfSection => 041,
-                    SysCalls.NtCreateProcess => 181,
-                    SysCalls.ZwProtectVirtualMemory => 081,
-                    SysCalls.ZwReadVirtualMemory => 0x40,
-                    SysCalls.NtCreateThread => 0x4f,
-                    SysCalls.NtUnmapViewOfSection => 0x2b,
-                    SysCalls.NtCreateUserProcess => 0xc4,
-                    SysCalls.ZwFreeVirtualMemory => 0x1f,
-                    SysCalls.NtQueueApcThread => 0x46,
-                    _ => throw new ArgumentException($"Unknown Syscall type: {syscallType}")
-                },
-                (int)WindowsVersions.Windows7_SP1 => syscallType switch
-                {
-                    SysCalls.NtOpenProcess => 039,
-                    SysCalls.NtCreateThreadEx => 189,
-                    SysCalls.NtWriteVirtualMemory => 059,
-                    SysCalls.ZwAllocateVirtualMemory => 025,
-                    SysCalls.NtCreateSection => 075,
-                    SysCalls.ZwMapViewOfSection => 041,
-                    SysCalls.NtCreateProcess => 181,
-                    SysCalls.ZwProtectVirtualMemory => 081,
+                    SysCalls.NtOpenProcess => 0x26,
+                    SysCalls.NtCreateThreadEx => 0xb1,
+                    SysCalls.NtWriteVirtualMemory => 0x3a,
+                    SysCalls.ZwAllocateVirtualMemory => 0x18,
+                    SysCalls.NtCreateSection => 0x4a,
+                    SysCalls.ZwMapViewOfSection => 0x28,
+                    SysCalls.NtCreateProcess => 0xab,
+                    SysCalls.ZwProtectVirtualMemory => 0x50,
                     SysCalls.ZwReadVirtualMemory => 0x3d,
                     SysCalls.NtCreateThread => 0x4f,
                     SysCalls.NtUnmapViewOfSection => 0x2b,
@@ -182,26 +99,26 @@ namespace Core.WindowsInternals
                     SysCalls.NtQueueApcThread => 0x43,
                     _ => throw new ArgumentException($"Unknown Syscall type: {syscallType}")
                 },
-                (int)WindowsVersions.Server2012_R2 => syscallType switch
+                (int)WindowsVersions._7601 => syscallType switch
                 {
-                    SysCalls.NtOpenProcess => 038,
-                    SysCalls.NtCreateThreadEx => 177,
-                    SysCalls.NtWriteVirtualMemory => 058,
-                    SysCalls.ZwAllocateVirtualMemory => 024,
-                    SysCalls.NtCreateSection => 074,
-                    SysCalls.ZwMapViewOfSection => 040,
-                    SysCalls.NtCreateProcess => 171,
-                    SysCalls.ZwProtectVirtualMemory => 080,
+                    SysCalls.NtOpenProcess => 0x24,
+                    SysCalls.NtCreateThreadEx => 0xa6,
+                    SysCalls.NtWriteVirtualMemory => 0x38,
+                    SysCalls.ZwAllocateVirtualMemory => 0x16,
+                    SysCalls.NtCreateSection => 0x48,
+                    SysCalls.ZwMapViewOfSection => 0x26,
+                    SysCalls.NtCreateProcess => 0xa0,
+                    SysCalls.ZwProtectVirtualMemory => 0x4e,
                     SysCalls.ZwReadVirtualMemory => 0x3d,
-                    SysCalls.NtCreateThread => 0x4f,
-                    SysCalls.NtUnmapViewOfSection => 0x2b,
+                    SysCalls.NtCreateThread => 0x4c,
+                    SysCalls.NtUnmapViewOfSection => 0x28,
                     SysCalls.NtCreateUserProcess => 0xab,
                     SysCalls.ZwFreeVirtualMemory => 0x1c,
                     SysCalls.NtQueueApcThread => 0x43,
                     _ => throw new ArgumentException($"Unknown Syscall type: {syscallType}")
                 },
                 _ => throw new NotSupportedException(
-                    $"Windows Build Number Not Supported: {osVersionInfo.dwBuildNumber}, get your friendly neighbourhood developer to update the syscalls in core.")
+                    $"Windows Build Number Not Supported: {osVersionInfo.dwBuildNumber}, run syscallsextractor and get your friendly neighbourhood developer to update the syscalls in core.")
             } - 1);
         }
     }
